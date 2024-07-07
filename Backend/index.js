6d2f4f8eef56073a90369fd8988ce9cca24c4bcd
Balajihero07 <balajisekar018@gmail.com>
2024-07-07 18:53:52 +0530

    changes

--- Backend/index.js
+++ Backend/index.js
@@ -41,7 +41,7 @@ app.get('/', (req, res) => {
 app.use('/api/users', userDataRoute);
 
 // Start the server
-const port = process.env.PORT || 3000;
+const port = process.env.APIURI || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
