12e797c474f51bd07f0c3f0acad98de8b46311dd
Balajihero07 <158764057+Balajihero07@users.noreply.github.com>
2024-07-07 21:51:40 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,47 +1,26 @@
 const express = require("express");
 const cors = require('cors');
+const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const userDataRoute = require("./routes/userDataRoute");
-
 dotenv.config();
-const app = express();
+app.use(cors());
+
+const userDataRoute = require("./routes/userDataRoute");
 
 // Middleware to parse JSON requests
 app.use(express.json());
-app.use(cors());
 
-// Fetch MongoDB URI from environment variable
-const mongoUri = process.env.MONGODB_URI;
-if (!mongoUri) {
-  console.error('MONGODB_URI environment variable is not defined.');
-  process.exit(1);
-}
+// Connect to MongoDB database (locally)
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected Successfully");
+    app.listen(process.env.PORT || 5000, (err) => {
+      if (err) console.log(err);
+      console.log(`running at port ${process.env.PORT}`);
+    });
+  });
+  
 
-// Connect to MongoDB
-mongoose.connect(mongoUri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  tls: true // Ensure this is set if required by your MongoDB Atlas setup
-})
-.then(() => {
-  console.log('Successfully connected to MongoDB');
-})
-.catch(err => {
-  console.error('Error connecting to MongoDB:', err);
-  process.exit(1);
-});
-
-// Define routes
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-// Attach user data routes
-app.use('/api/users', userDataRoute);
-
-// Start the server
-const port = process.env.APIURI || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// Attach routes
+app.use(userDataRoute);
